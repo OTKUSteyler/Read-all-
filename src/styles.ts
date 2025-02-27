882546a21688d4eca97fc925819527ffb2b36b76
OTKUSteyler <90369564+OTKUSteyler@users.noreply.github.com>
2025-02-27 23:33:49 +0100

    Update styles.ts

--- src/styles.ts
+++ src/styles.ts
@@ -1,21 +1,21 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
     container: {
-        position: "absolute",
+        position: 'absolute',
         top: 50,
         left: 10,
-        backgroundColor: "#7289DA",
+        backgroundColor: '#7289DA',
         padding: 10,
         borderRadius: 5,
     },
     button: {
         padding: 10,
-        backgroundColor: "#5865F2",
+        backgroundColor: '#5865F2',
         borderRadius: 5,
     },
     buttonText: {
-        color: "white",
-        fontWeight: "bold",
+        color: 'white',
+        fontWeight: 'bold',
     },
 });
