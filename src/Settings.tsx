c1d577dc42fa81b5593e8fa6c247510c5d01ac6e
OTKUSteyler <90369564+OTKUSteyler@users.noreply.github.com>
2025-02-27 18:28:23 +0100

    Update Settings.tsx

--- src/Settings.tsx
+++ src/Settings.tsx
@@ -4,9 +4,6 @@ import { Forms } from "@vendetta/ui/components";
 
 const { FormSwitch } = Forms;
 
-// Default storage setup
-storage.markDMs = storage.markDMs ?? true; // Enabled by default
-
 export default function SettingsPage() {
   return (
     <FormSwitch
