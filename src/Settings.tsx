283456c3de4c6d424be8145433cf7198cbc52f09
OTKUSteyler <90369564+OTKUSteyler@users.noreply.github.com>
2025-02-27 18:58:05 +0100

    Update Settings.tsx

--- src/Settings.tsx
+++ src/Settings.tsx
@@ -4,12 +4,12 @@ import { Forms } from "@vendetta/ui/components";
 
 const { FormSwitch } = Forms;
 
-export default function SettingsPage() {
-  return (
-    <FormSwitch
-      label="Mark DMs as Read"
-      value={storage.markDMs}
-      onValueChange={(value) => (storage.markDMs = value)}
-    />
-  );
-}
+export default () => (
+  <FormSwitch
+    label="Mark DMs as Read"
+    value={storage.markDMs}
+    onValueChange={(value) => {
+      storage.markDMs = value;
+    }}
+  />
+);
