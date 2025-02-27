afeb2e80fa2ba91ed6c125a478519507f5f464f0
OTKUSteyler <90369564+OTKUSteyler@users.noreply.github.com>
2025-02-27 23:31:31 +0100

    Update Settings.tsx

--- src/Settings.tsx
+++ src/Settings.tsx
@@ -1,22 +1,21 @@
-import { storage } from "@vendetta/plugin";
-import { useProxy } from "@vendetta/storage";
-import { ReactNative } from "@vendetta/metro/common";
-import { General } from "@vendetta/ui/components";
+import { StyleSheet } from "react-native";
 
-const { View } = ReactNative;
-const { FormSwitchRow } = General;
-
-export default function Settings() {
-    useProxy(storage);
-
-    return (
-        <View>
-            <FormSwitchRow
-                label="Mark DMs as Read"
-                subLabel="If enabled, direct messages will also be marked as read."
-                value={storage.markDMs ?? true}
-                onValueChange={(value) => (storage.markDMs = value)}
-            />
-        </View>
-    );
-}
+export const styles = StyleSheet.create({
+    container: {
+        position: "absolute",
+        top: 50,
+        left: 10,
+        backgroundColor: "#7289DA",
+        padding: 10,
+        borderRadius: 5,
+    },
+    button: {
+        padding: 10,
+        backgroundColor: "#5865F2",
+        borderRadius: 5,
+    },
+    buttonText: {
+        color: "white",
+        fontWeight: "bold",
+    },
+});
