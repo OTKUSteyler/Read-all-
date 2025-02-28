9e5f5d047d7ab7d20466054eb1cd9a5a38d66b2c
OTKUSteyler <90369564+OTKUSteyler@users.noreply.github.com>
2025-02-28 14:10:53 +0100

    Update styles.ts

--- src/styles.ts
+++ src/styles.ts
@@ -1,21 +1,18 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet } from "react-native";
 
-export const styles = StyleSheet.create({
-    container: {
-        position: 'absolute',
-        top: 50,
-        left: 10,
-        backgroundColor: '#7289DA',
-        padding: 10,
-        borderRadius: 5,
-    },
-    button: {
-        padding: 10,
-        backgroundColor: '#5865F2',
-        borderRadius: 5,
-    },
-    buttonText: {
-        color: 'white',
-        fontWeight: 'bold',
-    },
+export default StyleSheet.create({
+  container: {
+    padding: 10,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 10,
+  },
+  button: {
+    marginTop: 10,
+    padding: 10,
+  },
 });
