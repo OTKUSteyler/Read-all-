04aade6a82a1e91cfb1aecb7e279e242b1fe9efd
OTKUSteyler <90369564+OTKUSteyler@users.noreply.github.com>
2025-03-01 10:13:31 +0100

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,8 @@ import { showToast, ToastType } from "@vendetta/ui/toasts";
 export default {
   onLoad: () => {
     showToast("Read All Messages Plugin Loaded!", ToastType.SUCCESS);
-    console.log("Read All Messages Plugin Loaded!");
   },
   onUnload: () => {
     showToast("Plugin Unloaded!", ToastType.INFO);
-    console.log("Read All Messages Plugin Unloaded!");
   },
 };
