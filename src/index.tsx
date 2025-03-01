eb0a54b6a6abbc39f0a0300a553b73836d305c57
OTKUSteyler <90369564+OTKUSteyler@users.noreply.github.com>
2025-03-01 10:29:25 +0100

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
-import { showToast, ToastType } from "@vendetta/ui/toasts";
+import { showToast } from "@vendetta/ui/toasts";
 
 export default {
   onLoad: () => {
-    showToast("Read All Messages Plugin Enabled!", ToastType.SUCCESS);
+    showToast("Read All Messages Plugin Enabled!", { type: "success" });
     console.log("✅ Read All Messages Plugin Loaded!");
   },
   onUnload: () => {
-    showToast("Read All Messages Plugin Disabled!", ToastType.INFO);
+    showToast("Read All Messages Plugin Disabled!", { type: "info" });
     console.log("❌ Read All Messages Plugin Unloaded!");
   },
 };
