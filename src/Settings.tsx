f9fbb2e0cf57a0cefb2f6566a5fbb87c3cef01ce
OTKUSteyler <90369564+OTKUSteyler@users.noreply.github.com>
2025-03-01 18:38:38 +0100

    Update Settings.tsx

--- src/Settings.tsx
+++ src/Settings.tsx
@@ -1,15 +1,20 @@
 import { storage } from "@vendetta/plugin";
-import { React } from "@vendetta/metro/common";
+import { React, useState } from "@vendetta/metro/common";
 import { Forms } from "@vendetta/ui/components";
 
-export default () => (
-    <Forms.FormSection title="Read All Messages Plugin">
-        <Forms.FormSwitchRow
-            label="Enable 'Read All Messages' Overlay Button"
-            value={storage.enableReadAll}
-            onValueChange={(val) => {
-                storage.enableReadAll = val;
-            }}
-        />
-    </Forms.FormSection>
-);
+export default () => {
+    const [enabled, setEnabled] = useState(storage.enableReadAll);
+
+    return (
+        <Forms.FormSection title="Read All Messages Plugin">
+            <Forms.FormSwitchRow
+                label="Enable 'Read All Messages' Overlay Button"
+                value={enabled}
+                onValueChange={(val) => {
+                    storage.enableReadAll = val;
+                    setEnabled(val); // Immediately update the UI
+                }}
+            />
+        </Forms.FormSection>
+    );
+};
